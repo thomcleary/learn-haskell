35be6cbcd7afd69174c4d8dd80c375cc806e35dd
thomcleary <thomasclearydev@gmail.com>
2025-08-27 20:06:58 +0800

    Haskell MOOC: Exercise Set 7

--- haskell-mooc/exercises/Set7.hs
+++ haskell-mooc/exercises/Set7.hs
@@ -2,11 +2,11 @@
 
 module Set7 where
 
-import Mooc.Todo
 import Data.List
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.Monoid
 import Data.Semigroup
+import Mooc.Todo
 
 ------------------------------------------------------------------------------
 -- Ex 1: you'll find below the types Time, Distance and Velocity,
@@ -16,21 +16,21 @@ import Data.Semigroup
 -- Implement the functions below.
 
 data Distance = Distance Double
-  deriving (Show,Eq)
+  deriving (Show, Eq)
 
 data Time = Time Double
-  deriving (Show,Eq)
+  deriving (Show, Eq)
 
 data Velocity = Velocity Double
-  deriving (Show,Eq)
+  deriving (Show, Eq)
 
 -- velocity computes a velocity given a distance and a time
 velocity :: Distance -> Time -> Velocity
-velocity = todo
+velocity (Distance d) (Time t) = Velocity (d / t)
 
 -- travel computes a distance given a velocity and a time
 travel :: Velocity -> Time -> Distance
-travel = todo
+travel (Velocity v) (Time t) = Distance (v * t)
 
 ------------------------------------------------------------------------------
 -- Ex 2: let's implement a simple Set datatype. A Set is a list of
@@ -45,19 +45,24 @@ travel = todo
 --   add 1 (add 1 emptySet)  ==>  Set [1]
 
 data Set a = Set [a]
-  deriving (Show,Eq)
+  deriving (Show, Eq)
 
 -- emptySet is a set with no elements
 emptySet :: Set a
-emptySet = todo
+emptySet = Set []
 
 -- member tests if an element is in a set
-member :: Eq a => a -> Set a -> Bool
-member = todo
+member :: (Eq a) => a -> Set a -> Bool
+member x (Set []) = False
+member x (Set (head : tail)) = x == head || member x (Set tail)
 
 -- add a member to a set
-add :: a -> Set a -> Set a
-add = todo
+add :: (Ord a) => a -> Set a -> Set a
+add x (Set []) = Set [x]
+add x set@(Set (head : tail))
+  | x == head = set
+  | x < head = Set (x : head : tail)
+  | otherwise = add head (add x (Set tail))
 
 ------------------------------------------------------------------------------
 -- Ex 3: a state machine for baking a cake. The type Event represents
@@ -90,18 +95,27 @@ add = todo
 --   bake [AddEggs,AddFlour,Mix]  ==>  Error
 
 data Event = AddEggs | AddFlour | AddSugar | Mix | Bake
-  deriving (Eq,Show)
+  deriving (Eq, Show)
 
-data State = Start | Error | Finished
-  deriving (Eq,Show)
+data State = Start | Egged | Floured | Sweetened | Mixable | Bakeable | Error | Finished
+  deriving (Eq, Show)
 
-step = todo
+step Start AddEggs = Egged
+step Egged AddFlour = Floured
+step Egged AddSugar = Sweetened
+step Floured AddSugar = Mixable
+step Sweetened AddFlour = Mixable
+step Mixable Mix = Bakeable
+step Bakeable Bake = Finished
+step Finished _ = Finished
+step _ _ = Error
 
 -- do not edit this
 bake :: [Event] -> State
 bake events = go Start events
-  where go state [] = state
-        go state (e:es) = go (step state e) es
+  where
+    go state [] = state
+    go state (e : es) = go (step state e) es
 
 ------------------------------------------------------------------------------
 -- Ex 4: remember how the average function from Set4 couldn't really
@@ -114,8 +128,8 @@ bake events = go Start events
 --   average (1.0 :| [])  ==>  1.0
 --   average (1.0 :| [2.0,3.0])  ==>  2.0
 
-average :: Fractional a => NonEmpty a -> a
-average = todo
+average :: (Fractional a) => NonEmpty a -> a
+average xs = sum xs / fromIntegral (length xs)
 
 ------------------------------------------------------------------------------
 -- Ex 5: reverse a NonEmpty list.
@@ -123,7 +137,9 @@ average = todo
 -- PS. The Data.List.NonEmpty type has been imported for you
 
 reverseNonEmpty :: NonEmpty a -> NonEmpty a
-reverseNonEmpty = todo
+reverseNonEmpty (x :| xs) = head :| tail
+  where
+    (head : tail) = reverse (x : xs)
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement Semigroup instances for the Distance, Time and
@@ -135,6 +151,14 @@ reverseNonEmpty = todo
 -- velocity (Distance 50 <> Distance 10) (Time 1 <> Time 2)
 --    ==> Velocity 20
 
+instance Semigroup Distance where
+  (Distance x) <> (Distance y) = Distance (x + y)
+
+instance Semigroup Time where
+  (Time x) <> (Time y) = Time (x + y)
+
+instance Semigroup Velocity where
+  (Velocity x) <> (Velocity y) = Velocity (x + y)
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement a Monoid instance for the Set type from exercise 2.
@@ -144,6 +168,12 @@ reverseNonEmpty = todo
 --
 -- What are the class constraints for the instances?
 
+instance (Ord a) => Semigroup (Set a) where
+  (Set []) <> b = b
+  (Set (a : as)) <> b = Set as <> add a b
+
+instance (Ord a) => Monoid (Set a) where
+  mempty = emptySet
 
 ------------------------------------------------------------------------------
 -- Ex 8: below you'll find two different ways of representing
@@ -164,31 +194,32 @@ reverseNonEmpty = todo
 --   show2 (Subtract2 2 3) ==> "2-3"
 --   show2 (Multiply2 4 5) ==> "4*5"
 
-data Operation1 = Add1 Int Int
-                | Subtract1 Int Int
-  deriving Show
+data Operation1
+  = Add1 Int Int
+  | Subtract1 Int Int
+  deriving (Show)
 
 compute1 :: Operation1 -> Int
-compute1 (Add1 i j) = i+j
-compute1 (Subtract1 i j) = i-j
+compute1 (Add1 i j) = i + j
+compute1 (Subtract1 i j) = i - j
 
 show1 :: Operation1 -> String
 show1 = todo
 
 data Add2 = Add2 Int Int
-  deriving Show
+  deriving (Show)
+
 data Subtract2 = Subtract2 Int Int
-  deriving Show
+  deriving (Show)
 
 class Operation2 op where
   compute2 :: op -> Int
 
 instance Operation2 Add2 where
-  compute2 (Add2 i j) = i+j
+  compute2 (Add2 i j) = i + j
 
 instance Operation2 Subtract2 where
-  compute2 (Subtract2 i j) = i-j
-
+  compute2 (Subtract2 i j) = i - j
 
 ------------------------------------------------------------------------------
 -- Ex 9: validating passwords. Below you'll find a type
@@ -208,13 +239,13 @@ instance Operation2 Subtract2 where
 --   passwordAllowed "p4ss" (And (ContainsSome "1234") (MinimumLength 5)) ==> False
 --   passwordAllowed "p4ss" (Or (ContainsSome "1234") (MinimumLength 5)) ==> True
 
-data PasswordRequirement =
-  MinimumLength Int
-  | ContainsSome String    -- contains at least one of given characters
-  | DoesNotContain String  -- does not contain any of the given characters
+data PasswordRequirement
+  = MinimumLength Int
+  | ContainsSome String -- contains at least one of given characters
+  | DoesNotContain String -- does not contain any of the given characters
   | And PasswordRequirement PasswordRequirement -- and'ing two requirements
-  | Or PasswordRequirement PasswordRequirement  -- or'ing
-  deriving Show
+  | Or PasswordRequirement PasswordRequirement -- or'ing
+  deriving (Show)
 
 passwordAllowed :: String -> PasswordRequirement -> Bool
 passwordAllowed = todo
@@ -240,7 +271,7 @@ passwordAllowed = todo
 --
 
 data Arithmetic = Todo
-  deriving Show
+  deriving (Show)
 
 literal :: Integer -> Arithmetic
 literal = todo
