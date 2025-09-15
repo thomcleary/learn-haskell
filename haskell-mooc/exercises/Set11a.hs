eff6daa707f36655b27e1866c8375c4368edd5ca
thomcleary <thomasclearydev@gmail.com>
2025-09-15 11:52:55 +0800

    Haskell MOOC: Exercise Set 11a - Ex 2

--- haskell-mooc/exercises/Set11a.hs
+++ haskell-mooc/exercises/Set11a.hs
@@ -33,7 +33,8 @@ hello = do
 -- argument and prints a line "HELLO name".
 
 greet :: String -> IO ()
-greet name = todo
+greet name = do
+  putStrLn $ "HELLO " ++ name
 
 ------------------------------------------------------------------------------
 -- Ex 3: define the IO operation greet2 that reads a name from the
